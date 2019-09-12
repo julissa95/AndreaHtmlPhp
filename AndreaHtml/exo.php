cf8f1526228b363dc12fa8862cfa1f8c2bcd6a33
Julissa JOSEPH <54089454+julissa95@users.noreply.github.com>
2019-09-12 20:35:57 -0300

    Add files via upload

---
+++ AndreaHtml/exo.php
@@ -0,0 +1,13 @@
+<?php
+$nombre='Andrea';
+echo "Hola Mundo soy".$nombre;
+?>
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+  <title>Este es el titulo, no e ve</title>
+  </head>
+  <Body>
+   <p>Esta es mi pagina</p>
+   </body>
+   </html>
---
+++ AndreaHtml/exo2.php
@@ -0,0 +1,15 @@
+<?php
+$nombre='Andrea';
+echo "Hola Mundo soy".$nombre;
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Paginas web de Andrea</title>
+  </head>
+  <body>
+     <h1>Pagina web de Andrea</h1>
+     <h3><?php echo 'Hola Mundo,mi nombre es'.$nombre
+?></h3>
+   </body>
+   </html>
